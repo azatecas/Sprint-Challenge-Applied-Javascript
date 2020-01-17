7062fe907021f61fb895ecc5a2a2eec95cc5b866
Luis PM <luisp237@gmail.com>
2020-01-17 11:28:50 -0500

    add Header() with div,span,h1,span elements with content and attached to .header-container

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+    let headerDiv = document.createElement('div');
+    let dateSpan = document.createElement('span');
+    let headerH1 = document.createElement('h1');
+    let tempSpan = document.createElement('span');
+
+    //adding classes to elements
+    headerDiv.classList.add('header');
+    dateSpan.classList.add('date');
+    tempSpan.classList.add('temp');
+
+    //setting text content
+    dateSpan.textContent = 'SMARCH 28, 2019';
+    headerH1.textContent = 'Lambda Times';
+    tempSpan.textContent = '98 &#xb0';
+
+    //appending elements to top div
+    headerDiv.append(dateSpan);
+    headerDiv.append(headerH1);
+    headerDiv.append(tempSpan);
+
+
+    return headerDiv;
+}
+
+const headerContainer = document.querySelector('.header-container');
+const myHeader = Header();
+
+headerContainer.append(myHeader);
